dd8edaf2e4224ec7600b20bfb1deb882a957c717
KTwilliams3 <williamsk@hssd.net>
2022-07-12 14:08:41 -0500

    FirstCommit

--- ColorApp/ColorApp/MainPage.xaml.cs
+++ ColorApp/ColorApp/MainPage.xaml.cs
@@ -2,23 +2,28 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void MagentaClicked(object sender, EventArgs e)
 	{
-		count++;
+		cPage.Background = Color.Parse("Magenta");
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+	private void OrangeClicked(object sender, EventArgs e)
+	{
+        cPage.Background = Color.Parse("Orange");
+
+    }
+
+	private void ResetClicked(object sender, EventArgs e)
+	{
+        cPage.Background = Color.Parse("Black");
+
+    }
 }
 
